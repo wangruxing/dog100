ea43021f3e0d0a1e8cac8a75be7ebf1e9e41c4ea
h94373035 <h94373035@mailst.cjcu.edu.tw>
2017-03-03 21:32:00 +0800

    no message

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 var linebot = require('linebot');
 var express = require('express');
 
+var linebot = require('linebot');
+var express = require('express');
+
 var bot = linebot({
-  channelId:1499626788,
-  channelSecret:	9e95d637a0fb0ba08eebd84274a2da18,
-  channelAccessToken:yWRRwIbq7uyu0MSrYBCRAvrTSK7bf9s76GSLoOGGpng+eaDO92YK9AWjj7159ASe3oPz7zkui9PQrOqfAo7MHhYx94dREPG9QIlVwSHfa3I1rUsqhyOO9mQGdj9FU2FA8uFBFJy0Kv24hWilGS0xwwdB04t89/1O/w1cDnyilFU=});
+  channelId: channel Id,
+  channelSecret: channel Secret,
+  channelAccessToken: channel Access Token
+});
 bot.on('message', function(event) {
   console.log(event);
 });
 
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+http.createServer(app).listen(app.get('port'),
+  function(){
+    console.log("Express server listening on port " + app.get('port'));
 });
+
+app.set('port', process.env.PORT || 3000);
+
+var express = require("express");
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(app.get('port'));
+
